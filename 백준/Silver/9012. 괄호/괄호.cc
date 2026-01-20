5db444c14c7f48af9aac092a117a3eacdb6b58b8
sieunBae00 <20241217@sungshin.ac.kr>
2026-01-20 22:02:42 +0900

    [Silver IV] Title: 괄호, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+#include <stack>
+
+int main(){
+    int N;
+    cin >> N;
+    stack<char> st;
+    bool success = true;
+    
+    for(int i=0; i<N; i++){
+        success = true;
+        while(!st.empty()){
+            st.pop();
+        }
+        
+        string s;
+        cin >> s;
+        
+        for(int j=0; j<s.length(); j++){
+            char c = s.at(j);
+            
+            if(c == '('){
+                st.push(c);
+            }
+            else if(c == ')'){
+                if(st.empty()) {
+                    success = false;
+                    break;
+                }
+                else if(st.top() == '('){
+                    st.pop();
+                }
+                else{
+                    success = false;
+                    break;
+                }
+            }
+        }
+        
+        if((success == true) && st.empty()){
+            cout << "YES" << '\n';
+        }
+        else{
+            cout << "NO" << '\n';
+        }
+    }
+}
