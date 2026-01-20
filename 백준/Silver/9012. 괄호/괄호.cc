9a489c19a5ef3535caaa330bfdc35c8b10d112f3
sieunBae00 <20241217@sungshin.ac.kr>
2026-01-20 23:19:16 +0900

    [Silver IV] Title: 괄호, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.cc
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -1,45 +1,28 @@
 #include <iostream>
 using namespace std;
-#include <stack>
 
-int main(){
-    int N;
-    cin >> N;
-    stack<char> st;
-    bool success = true;
-    
-    for(int i=0; i<N; i++){
-        success = true;
-        while(!st.empty()){
-            st.pop();
-        }
-        
-        string s;
-        cin >> s;
-        
-        for(int j=0; j<s.length(); j++){
-            char c = s.at(j);
-            
-            if(c == '('){
-                st.push(c);
-            }
-            else if(c == ')'){
-                if(st.empty()) {
-                    success = false;
-                    break;
-                }
-                else if(st.top() == '('){
-                    st.pop();
-                }
-            }
-        }
-        
-        if((success == true) && st.empty()){
-            cout << "YES" << '\n';
-        }
-        else{
-            cout << "NO" << '\n';
-        }
-    }
-    return 0;
+int main() {
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		int ans = 0;
+		bool success = true;
+
+		string s;
+		cin >> s;
+
+		for (char c : s) {
+			if (c == '(') ans++;
+			else {
+				ans--;
+				if (ans < 0) {
+					success = false;
+					break;
+				}
+			}
+		}
+		if ((success == true) && ans == 0) cout << "YES" << endl;
+		else cout << "NO" << endl;
+	}
 }
