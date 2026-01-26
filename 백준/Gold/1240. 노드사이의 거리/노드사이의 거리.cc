ed6af2e0c369682911288510b90b2e229f14aa10
sieunBae00 <20241217@sungshin.ac.kr>
2026-01-27 02:54:02 +0900

    [Gold V] Title: 노드사이의 거리, Time: 40 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+#include<vector>
+
+typedef pair<int, int> p;
+
+vector<vector<p>> dist;
+vector<bool> visited;
+
+int ans;
+
+void trip(int s, int e, int sum) {
+	if (s == e) {
+		ans = sum;
+		return;
+	}
+
+	visited[s] = true;
+
+	for (p next : dist[s]) {
+		if (!visited[next.first]) {
+			trip(next.first, e, sum + next.second);
+		}
+	}
+}
+
+
+int main() {
+	int N, M;
+	cin >> N >> M;
+
+	dist.resize(N + 1);
+
+	for (int i = 0; i < N - 1; i++) {
+		int u, v, d;
+		cin >> u >> v >> d;
+
+		dist[u].push_back(make_pair(v, d));
+		dist[v].push_back(make_pair(u, d));
+	}
+
+	for (int i = 0; i < M; i++) {
+		int s, e;
+		cin >> s >> e;
+
+		visited.assign(N+1, false);
+
+		ans = 0;
+		trip(s, e, 0);
+		cout << ans << "\n";
+	}
+
+	return 0;
+}
