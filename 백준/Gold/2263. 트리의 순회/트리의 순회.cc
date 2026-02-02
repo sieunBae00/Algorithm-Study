637cb1c20de3ca00157af99193aab9a4a6939ce9
sieunBae00 <20241217@sungshin.ac.kr>
2026-02-03 04:13:12 +0900

    Update 트리의 순회.cc

--- 백준/Gold/2263. 트리의 순회/트리의 순회.cc
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.cc
@@ -2,46 +2,61 @@
 using namespace std;
 #include <vector>
 
-const int MAX = 100001;
-
-int n;
-int inorder[MAX];
-int postorder[MAX];
-int position[MAX]; 
+vector<int> inorder;
+vector<int> postorder;
+vector<int> ans;
 
 void solve(int in_start, int in_end, int post_start, int post_end) {
-    if (in_start > in_end || post_start > post_end) return;
+	if ((in_end < in_start) || (post_end < post_start)) return;
 
-    int root = postorder[post_end];
+	int root = postorder[post_end];
+	cout << root << " ";
 
-    cout << root << " ";
+	int flag;
+	for (int i = in_start; i <= in_end; i++) {
+		if (inorder[i] == root) {
+			flag = i;
+			break;
+		}
+	}
 
-    int root_idx = position[root];
+	int L_size = (flag - 1) - in_start + 1;
+	int R_size = in_end - (flag + 1);
 
-    int left_size = root_idx - in_start;
 
-    solve(in_start, root_idx - 1, post_start, post_start + left_size - 1);
+	solve(in_start, flag - 1, post_start, post_start + L_size - 1);
+	solve(flag + 1, in_end, post_start + L_size, post_start + L_size + 1 + R_size - 1);
 
-    solve(root_idx + 1, in_end, post_start + left_size, post_end - 1);
+	return;
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    cin >> n;
+	int n;
+	cin >> n;
 
-    for (int i = 0; i < n; i++) {
-        cin >> inorder[i];
-        position[inorder[i]] = i;
-    }
+	inorder.resize(n + 1);
+	postorder.resize(n + 1);
 
-    for (int i = 0; i < n; i++) {
-        cin >> postorder[i];
-    }
 
-    solve(0, n - 1, 0, n - 1);
+	for (int i = 1; i <= n; i++) {
+		int num;
+		cin >> num;
 
-    return 0;
+		inorder[i] = num;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		int num;
+		cin >> num;
+
+		postorder[i] = num;
+	}
+
+	solve(1, n, 1, n);
+
+	return 0;
 }
