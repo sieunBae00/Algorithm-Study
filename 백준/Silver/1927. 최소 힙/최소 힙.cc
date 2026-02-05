19da9309a2ad48129ac11a71384e161ec4f5efca
sieunBae00 <20241217@sungshin.ac.kr>
2026-02-05 21:02:43 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+#include <queue>
+#include <vector>
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, x;
+	priority_queue<int, vector<int>, greater<int>> pq;
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cin >> x;
+		if (x == 0) {
+			if (pq.empty()) cout << 0 << '\n';
+			else {
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+		}
+		else {
+			pq.push(x); 
+		}
+	}
+
+	return 0;
+}
