4a70ffb4aaa4bd5e09149bed1d7665831cc4ad0f
sieunBae00 <20241217@sungshin.ac.kr>
2026-02-21 18:02:27 +0900

    Refactor family relationship calculation logic

--- 백준/Silver/2644. 촌수계산/촌수계산.cc
+++ 백준/Silver/2644. 촌수계산/촌수계산.cc
@@ -1,54 +1,52 @@
 #include <iostream>
 using namespace std;
-#include <vector>
-#include <queue>
+#include<vector>
+
+vector<vector<int>> family;
+vector<bool> visited;
+
+int findFamily(int st, int end, int ans) {
+	visited[st] = true;
+
+	if (st == end) {
+		return ans; // 올바른 길 찾음
+	}
+
+	for (int next : family[st]) {
+		if (!visited[next]) {
+			int res = findFamily(next, end, ans+1);
+			if (res != -1) return res; //부하가 올바른 길을 찾았다면(-1 아닌 것 확인), 다른 길 살펴볼 필요 없이 상사에게 보고
+		}
+	}
+
+	return -1; // 아무리 해도 올바른 길 못 찾음
+}
 
 int main() {
-
-	ios_base::sync_with_stdio(false);	cin.tie(NULL);
-	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
 	int n;
 	cin >> n;
 
-	int start, end;
-	cin >> start >> end;
+	family.resize(n + 1);
+	visited.resize(n + 1, false);
+
+	int st, end;
+	cin >> st >> end;
 
 	int m;
 	cin >> m;
 
-	vector<vector<int>> adj(n + 1);
-
 	for (int i = 0; i < m; i++) {
-		int u, v;
-		cin >> u >> v;
+		int x, y;
+		cin >> x >> y;
 
-		adj[u].push_back(v);
-		adj[v].push_back(u);
-
+		family[x].push_back(y);
+		family[y].push_back(x);
 	}
 
-	vector<int> dist(n + 1, -1);
-	queue<int> q;
-	q.push(start);
-	dist[start] = 0;
-
-	while (!q.empty()) {
-		int curr = q.front();
-		q.pop();
-
-		if (curr == end) {
-			break;
-		}
-
-		for (int next : adj[curr]) {
-			if (dist[next] == -1) {
-				dist[next] = dist[curr] + 1;
-				q.push(next);
-			}
-		}
-	}
-	cout << dist[end] << '\n';
+	cout << findFamily(st, end, 0) << '\n';
 
 	return 0;
 }
